523c70354fb0e55a349f0d797532032c25f23ffb
Hilman Fitriana <hilmanfitriana39@gmail.com>
2025-10-05 00:00:00 +0700

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/split-bill/', // Menyesuaikan base path
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 })
